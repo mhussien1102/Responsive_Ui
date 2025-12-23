f707e1df8c03c728705fcae52b902a1458162030
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2025-12-23 19:04:01 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:responsive_ui/views/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeView());
+  }
+}
---
+++ lib/views/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: Icon(Icons.menu, color: Colors.white),
+      ),
+    );
+  }
+}
