cab11f4759acbfdd9d3320000a207ad3b3ba545e
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2026-01-01 13:47:17 +0200

    34.Hide Appbar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -10,25 +10,58 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
-      drawer: CustomDrawer(),
-      backgroundColor: Color(0xffDBDBDB),
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        leading: GestureDetector(
-          onTap: () {
-            scaffoldKey.currentState!.openDrawer();
-          },
-          child: Icon(Icons.menu, color: Colors.white),
-        ),
-      ),
-
-      body: HomeViewBody(),
+      drawer: const CustomDrawer(),
+      backgroundColor: const Color(0xffDBDBDB),
+      appBar: buildAppBar(context),
+      body: const HomeViewBody(),
     );
   }
+
+  AppBar? buildAppBar(BuildContext context) {
+    return MediaQuery.sizeOf(context).width < 900
+        ? AppBar(
+            backgroundColor: Colors.black,
+            leading: GestureDetector(
+              onTap: scaffoldKey.currentState!.openDrawer,
+              child: const Icon(Icons.menu, color: Colors.white),
+            ),
+          )
+        : null;
+  }
 }
+
+//
+// class CustomAdaptiveAppBar extends StatelessWidget
+//     implements PreferredSizeWidget {
+//   final VoidCallback onMenuTap;
+//
+//   const CustomAdaptiveAppBar({super.key, required this.onMenuTap});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return LayoutBuilder(
+//       builder: (context, constraints) {
+//         if (constraints.maxWidth < 900) {
+//           return AppBar(
+//             backgroundColor: Colors.black,
+//             leading: GestureDetector(
+//               onTap: onMenuTap,
+//               child: const Icon(Icons.menu, color: Colors.white),
+//             ),
+//           );
+//         } else {
+//           return SizedBox();
+//         }
+//       },
+//     );
+//   }
+//
+//   @override
+//   Size get preferredSize => const Size.fromHeight(56);
+// }
